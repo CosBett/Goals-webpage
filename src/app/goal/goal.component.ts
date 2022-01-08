2932306eae3c88936cafed178cc8ed43226b9bfb
CosBett <cosmasbett9@gmail.com>
2022-01-08 12:40:50 +0300

    add css styling

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -4,7 +4,9 @@ import { Goal } from '../goal';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.css']
+  styleUrls: ['./goal.component.css'],
+  styles: ['h6 {color:red}']
+
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
@@ -28,7 +30,12 @@ export class GoalComponent implements OnInit {
 
     }
   }
-
+  addNewGoal(goal: Goal) {
+    let goalLength = this.goals.length;
+    goal.id = goalLength + 1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
 
   constructor() { }
 
